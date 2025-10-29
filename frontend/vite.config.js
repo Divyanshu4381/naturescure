8ec7a9bccf502b0fec798217f0abf48bfcc10448
Saral224x2 <testtmaill224x2@gmail.com>
2025-10-29 19:58:48 +0530

    Add Vercel configuration and update Vite server settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,29 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    // host:"0.0.0.0",
-    fs:{
-      strict:false
+  server: {
+    // host: "0.0.0.0",
+    fs: {
+      strict: false
     },
-    proxy:{
-      '/api':"https://naturescure.vercel.app"
+    proxy: {
+      '/api': "https://naturescure.vercel.app"
     }
   },
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          // Agar aap koi aur large packages use kar rahe hain to yahan add karein
+          // example: 
+          // ui: ['react-router-dom'],
+          // utils: ['axios', 'lodash']
+        }
+      }
+    }
+  }
 })
